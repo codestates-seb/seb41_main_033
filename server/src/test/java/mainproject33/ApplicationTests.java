7cecee6fb15495de12c28b126252f744e9d613a7
DreamChaserDeekay <deekayshin@gmail.com>
2023-01-05 15:39:16 +0900

    first commit

---
+++ server/src/test/java/mainproject33/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mainproject33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

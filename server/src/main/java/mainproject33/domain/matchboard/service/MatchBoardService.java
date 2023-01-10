e0667e190a37178de91e67e17ee6dd4cded584f4
DreamChaserDeekay <110885981+DreamChaserDeekay@users.noreply.github.com>
2023-01-10 16:18:28 +0900

    Feat/be-match (#18)
    * [BE feat] #1 매치 게시글 CRUD
    1. MatchBoardController.java RestAPI 엔드포인트 설정
    2. MatchBoardService.java CRUD Business Logic 구현
    3. Pagination 구현 (default : page = 1, size = 10)
       - query parameter 로 page, size 값 조정 가능
       - keyword 있는 경우 : 매칭 게시글 title, content 에서 검색어를 찾아 id ASC 정렬
       - keyword 없는 경우 : 전체 매칭 게시글 id ASC 정렬
    
    * Auditable.java @Setter annotation 추가
    
    * application.yml 은 개발 단계에서 각자 설정이 다를 수 있으므로 no commit
    
    * [BE feat] #1 매치 게시글 CRUD
    1. MatchBoardService.java 의 pagination 정렬 기준 DESC(최신순)으로 변경
    2. MatchBoardDto.java validation 설정
       - title : max 30자
       - content : max 500자
    3. Exception Handling 클래스 생성
       - ErrorResponse.java
       - ExceptionAdvice.java
       - ExceptionMessage.java(커스텀 ExceptionMessage 는 해당 클래스에서 자유롭게 생성 / 활용 해주시면 될 것 같습니다)
    4. 로그 기능 추가
       - SimpleLoggingAop.java 에서 포인트 컷을 지정하면 해당하는 메소드들의
         1) Method Name
         2) Parameter
         3) Return Object
         를 로그로 확인할 수 있음
    
    * [BE feat] #1 매치 게시글 CRUD
    1. @EnableJpaAuditing annotation 추가 (setCreatedAt, setModifiedAt 삭제)
    
    * [BE feat] #1 매치 게시글 CRUD
    1. MatchBoard.java, MatchBoardDto.java 필드값 추가 : tags (데이터타입 : List<String>)
       - nullable (필수 값 x)
       - 앞에 "#"이 꼭 있어야 함
       - "#" 제외 영문 소문자 15자 이하, 한글 6자 이하, 띄어쓰기 x
         -> Custom Validator (@Tags.java) 이용
       - tag 갯수는 최대 5개 까지 허용
    
    * [BE feat] #1 매치 게시글 CRUD
    1. tags Size : 5 -> 3으로 수정 (화면 정의서 내용 반영)
    2. pagination size : 10 -> 8로 수정 (화면 정의서 내용 반영)
    3. MatchBoardDto.java, MatchBoard.java 필드값 추가
       - game (데이터타입 : GameDB)
       - team (데이터타입 : int)

---
+++ server/src/main/java/mainproject33/domain/matchboard/service/MatchBoardService.java
@@ -0,0 +1,74 @@
+package mainproject33.domain.matchboard.service;
+
+import lombok.RequiredArgsConstructor;
+import mainproject33.domain.matchboard.entity.MatchBoard;
+import mainproject33.domain.matchboard.repository.MatchBoardRepository;
+import mainproject33.global.exception.ExceptionMessage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MatchBoardService {
+
+    private final MatchBoardRepository matchBoardRepository;
+
+    public MatchBoard createMatchBoard(MatchBoard matchBoard) {
+        MatchBoard createdMatchBoard = new MatchBoard();
+
+        createdMatchBoard.setTitle(matchBoard.getTitle());
+        createdMatchBoard.setContent(matchBoard.getContent());
+        createdMatchBoard.setGame(matchBoard.getGame()); // TODO : Game DB에 있는 객체를 가지고 와서 저장 (mapper 에서 처리)
+        createdMatchBoard.setTeam(matchBoard.getTeam());
+        Optional.ofNullable(matchBoard.getTags())
+                .ifPresent(tags -> createdMatchBoard.setTags(tags));
+
+        return matchBoardRepository.save(createdMatchBoard);
+    }
+
+    public MatchBoard updateMatchBoard(MatchBoard matchBoard) {
+        MatchBoard findMatchBoard = matchBoardRepository.findById(matchBoard.getId())
+                .orElseThrow(()
+                        -> new NoSuchElementException(ExceptionMessage.MATCH_BOARD_NOT_FOUND.get()));
+
+        Optional.ofNullable(matchBoard.getTitle())
+                .ifPresent(title -> findMatchBoard.setTitle(title));
+        Optional.ofNullable(matchBoard.getContent())
+                .ifPresent(content -> findMatchBoard.setContent(content));
+        Optional.ofNullable(matchBoard.getTags())
+                .ifPresent(tags -> findMatchBoard.setTags(tags));
+        Optional.ofNullable(matchBoard.getGame())
+                .ifPresent(game -> findMatchBoard.setGame(game));
+        if (matchBoard.getTeam() != 0) findMatchBoard.setTeam(matchBoard.getTeam());
+
+        return matchBoardRepository.save(findMatchBoard);
+    }
+
+    public MatchBoard readMatchBoard(long id) {
+        MatchBoard findMatchBoard = matchBoardRepository.findById(id)
+                .orElseThrow(()
+                        -> new NoSuchElementException(ExceptionMessage.MATCH_BOARD_NOT_FOUND.get()));
+
+        return findMatchBoard;
+    }
+
+    public Page<MatchBoard> readMatchBoards(String keyword, Pageable pageable) {
+        if (keyword == null) {
+            return matchBoardRepository.findAll(pageable);
+        } else {
+            return matchBoardRepository.findByKeyword(keyword, pageable);
+        }
+    }
+
+    public void deleteMatchBoard(long id) {
+        MatchBoard findMatchBoard = matchBoardRepository.findById(id)
+                .orElseThrow(()
+                        -> new NoSuchElementException(ExceptionMessage.MATCH_BOARD_NOT_FOUND.get()));
+
+        matchBoardRepository.delete(findMatchBoard);
+    }
+}
--- server/src/main/java/mainproject33/global/audit/Auditable.java
+++ server/src/main/java/mainproject33/global/audit/Auditable.java
@@ -1,6 +1,7 @@
 package mainproject33.global.audit;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,7 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class Auditable

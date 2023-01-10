58d2b33c76cb9bbe44275811087e915aa050bccb
SEB_FE_41_김소라 <110882351+kimsorara@users.noreply.github.com>
2023-01-10 11:38:17 +0900

    [FE feat] #6 route 경로 설정 (#9)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router basename={process.env.PUBLIC_URL}>
+        {/* <Header />
+        <Nav /> */}
+        <Routes>
+          {/* <Route path="/" element={<Matching/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path={`/:${user-id}`} element={<Profile />} />
+          <Route path="/storyfriend" element={<Story />} />
+          <Route path={`/:${user-id}/edit`} element={<Profile />} />
+          <Route path={`/:${user-id}/:${board-id}`} element={<StoryDetail />} /> */}
+          {/* <Route path="/storyall" element={<Story />} />
+          <Route path={`/${board - id}`} element={<MatchingDetail />} />
+          <Route path="/quit" element={<Quit />} /> */}
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

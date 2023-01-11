324929464502bdc43c831a49415f2e35ec4c64a7
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2023-01-11 10:47:14 +0900

    [Feat/fe-navView] Nav 컴포넌트 구현 (#26)
    * [FE feat] #8 레이아웃 관련 App.js 수정, Header, Nav 컴포넌트 추가
    
    * [FE feat] #8 전역스타일 추가
    
    * [FE feat] #8 index.css 삭제
    
    * [FE feat] #8 폰트 추가
    
    * [FE feat] #21 Nav 컴포넌트 생성
    
    * [FE misc] #21 레이아웃 수정
    
    * [FE design] 전역스타일 추가 및 수정
    
    * [FE misc] #21 Nav 컴포넌트 생성
    
    * [FE feat] #21 필요한 아이콘 svg 추가

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,15 +1,128 @@
 import styled from "styled-components";
+import { ReactComponent as LogoImg } from "./../assets/Logo.svg";
+import { ReactComponent as MenuIconMatch } from "./../assets/handshakeIcon.svg";
+import { ReactComponent as MenuIconStory } from "./../assets/boardIcon.svg";
+import { ReactComponent as MenuIconGame } from "./../assets/gameIcon.svg";
+import { ReactComponent as MenuIconProfile } from "./../assets/personIcon.svg";
 
 const NavWrap = styled.nav`
+	position: relative;
 	flex: none;
 	width: 200px;
 	background: var(--bg-card-color);
+
+	.copy {
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		padding: 48px 16px;
+		text-align: center;
+		font-size: var(--font-caption-size);
+	}
+`;
+
+const LogoWrap = styled.div`
+	position: sticky;
+	width: 100%;
+	height: 112px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	a {
+		display: block;
+		width: 130px;
+		height: 35px;
+		svg {
+			width: 100%;
+			height: 100%;
+		}
+	}
+`;
+
+const Menu = styled.ul`
+	position: sticky;
+	width: 100%;
+	padding: 0 16px;
+	li {
+		width: 100%;
+		border-radius: var(--border-raidus-md);
+		color: var(--font-color);
+		a {
+			display: flex;
+			align-items: center;
+			padding: 16px;
+			color: inherit;
+			svg {
+				width: 24px;
+				height: 24px;
+				margin-right: 12px;
+				fill: var(--font-color);
+				path {
+					fill: inherit;
+				}
+			}
+			span {
+				font-size: var(--font-body2-size);
+			}
+		}
+		a:hover {
+			color: var(--grey);
+			path {
+				fill: var(--grey);
+			}
+		}
+	}
+	li.active {
+		background: var(--black);
+		color: var(--primary-color);
+		svg {
+			fill: var(--primary-color);
+		}
+        a:hover {
+			color: var(--primary-color);
+			path {
+				fill: var(--primary-color);
+			}
+		}
+	}
 `;
 
 const Nav = () => {
 	return (
 		<NavWrap>
-			<div>Nav</div>
+			<LogoWrap>
+				<a href="" title="GAMETO">
+					<LogoImg />
+				</a>
+			</LogoWrap>
+			<Menu>
+				<li className="active">
+					<a href="none">
+						<MenuIconMatch />
+						<span>매칭하기</span>
+					</a>
+				</li>
+				<li>
+					<a href="none">
+						<MenuIconStory />
+						<span>스토리</span>
+					</a>
+				</li>
+				<li>
+					<a href="none">
+						<MenuIconGame />
+						<span>오늘뭐하지?</span>
+					</a>
+				</li>
+				<li>
+					<a href="none">
+						<MenuIconProfile />
+						<span>마이프로필</span>
+					</a>
+				</li>
+			</Menu>
+			<p className="copy">© 맑게고인물 team33 2023</p>
 		</NavWrap>
 	);
 };

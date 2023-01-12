4bb2180a61e7086b9c0b546285a75e9702ada1c8
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2023-01-12 15:40:37 +0900

    feat/fe-headerView (#42)
    * [FE feat] #8 레이아웃 관련 App.js 수정, Header, Nav 컴포넌트 추가
    
    * [FE feat] #8 전역스타일 추가
    
    * [FE feat] #8 index.css 삭제
    
    * [FE feat] #8 폰트 추가
    
    * [FE feat] #21 Nav 컴포넌트 생성
    
    * [FE misc] #21 레이아웃 수정
    
    * [FE design] 전역스타일 추가 및 수정
    
    * [FE misc] #21 Nav 컴포넌트 생성
    
    * [FE feat] #21 필요한 아이콘 svg 추가
    
    * [FE feat] #27 svg아이콘 추가
    
    * [FE feat] #27 Header 컴포넌트 생성
    
    * [FE fix] Nav컴포넌트 레이아웃 수정
    
    * [FE design] 버튼 hover 효과 추가
    
    * [FE misc] 컴포넌트 test 후 수정
    
    * [FE fix] 임시 test 문구 삭제
    
    * [FE misc] 이전 파일 삭제
    
    * [FE feat] #27 SearchBar 분리
    
    * [FE design] #27 레이아웃 위쪽 여백 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,59 @@
 import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./style.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Login from "./pages/Login";
-import "./style.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MatchingWrite from "./pages/MatchingWrite";
+
 const Wrap = styled.div`
-  display: flex;
-  flex: 1;
-  width: 100%;
-  background: var(--bg-color);
-
-  .card {
-    height: 900px;
-  }
+	display: flex;
+	flex: 1;
+	width: 100%;
+	background: var(--bg-color);
 `;
+
 const MainWrap = styled.section`
-  display: flex;
-  flex: 1;
-  position: relative;
-  padding: 112px 0 0 0;
-  width: calc(100% - 200px);
+	display: flex;
+	flex: 1;
+	position: relative;
+	padding: 112px 0 0 0;
+	width: calc(100% - 200px);
 
-  .container {
-    width: 100%;
-    max-width: 1040px;
-    padding: 0 32px;
-    margin: 0 auto;
-  }
+	.container {
+		width: 100%;
+		max-width: 1040px;
+		padding: 0 32px;
+		margin: 48px auto 48px auto;
+	}
 `;
-
-function App() {
-  return (
-    <Wrap>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Nav />
-        <MainWrap>
-          <Header />
-          {/* 아래 main 안에 각 페이지가 들어갑니다. */}
-          <main className="container">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              {/* <Route path="/" element={<Matching/>} />
+const App = () => {
+	return (
+		<Wrap>
+			<Router basename={process.env.PUBLIC_URL}>
+				<Nav />
+				<MainWrap>
+					<Header />
+					{/* 아래 main 안에 각 페이지가 들어갑니다. */}
+					<main className="container">
+						<Routes>
+							<Route path="/login" element={<Login />} />
+							{/* <Route path="/" element={<Matching/>} />
           <Route path="/signup" element={<Signup />} />
           <Route path={`/:${user-id}`} element={<Profile />} />
           <Route path="/storyfriend" element={<Story />} />
           <Route path={`/:${user-id}/edit`} element={<Profile />} />
           <Route path={`/:${user-id}/:${board-id}`} element={<StoryDetail />} /> */}
-              {/* <Route path="/storyall" element={<Story />} />
+							{/* <Route path="/storyall" element={<Story />} />
           <Route path={`/${board - id}`} element={<MatchingDetail />} />
           <Route path="/quit" element={<Quit />} /> */}
-              <Route path="/matchwrite" element={<MatchingWrite />} />
-            </Routes>
-          </main>
-        </MainWrap>
-      </Router>
-    </Wrap>
-  );
-}
+							<Route path="/matchwrite" element={<MatchingWrite />} />
+						</Routes>
+					</main>
+				</MainWrap>
+			</Router>
+		</Wrap>
+	);
+};
 
 export default App;

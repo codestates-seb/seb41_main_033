2b2c121cd975db7ffd6bfc65565ec8e4508d0a4b
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2023-01-13 15:09:50 +0900

    [feat/fe-storyView] 유저게시글 화면 구현 (#52)
    * [FE feat] #8 레이아웃 관련 App.js 수정, Header, Nav 컴포넌트 추가
    
    * [FE feat] #8 전역스타일 추가
    
    * [FE feat] #8 index.css 삭제
    
    * [FE feat] #8 폰트 추가
    
    * [FE feat] #21 Nav 컴포넌트 생성
    
    * [FE misc] #21 레이아웃 수정
    
    * [FE design] 전역스타일 추가 및 수정
    
    * [FE misc] #21 Nav 컴포넌트 생성
    
    * [FE feat] #21 필요한 아이콘 svg 추가
    
    * [FE feat] #27 svg아이콘 추가
    
    * [FE feat] #27 Header 컴포넌트 생성
    
    * [FE fix] Nav컴포넌트 레이아웃 수정
    
    * [FE design] 버튼 hover 효과 추가
    
    * [FE misc] 컴포넌트 test 후 수정
    
    * [FE fix] 임시 test 문구 삭제
    
    * [FE feat] #40 유저게시글 목록 개별메시지 컴포넌트 구현
    
    * [FE feat] #40 유저게시글 공통 페이지 컴포넌트 view 구현
    
    * [FE feat] #40 유저게시글 더미데이터 추가
    
    * [FE feat] #40 작성하기 플로트 버튼 컴포넌트 연결
    
    * [FE refactor] #40 불필요한 코드 수정
    
    * [FE misc] #40 data id추가
    
    * [FE misc] #40 Story 라우터 연결
    
    * [FE feat] #40 작성하기 플로트 버튼 컴포넌트 작업
    
    * [FE feat] #40 컴포넌트 라우터 연결

--- client/src/App.js
+++ client/src/App.js
@@ -5,57 +5,62 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Login from "./pages/Login";
 import MatchingWrite from "./pages/MatchingWrite";
+import Story from "./pages/Story";
 import Matching from "./pages/Matching";
 import MatchingDetail from "./pages/MatchingDetail";
+import Profile from "./pages/Profile";
+
 const Wrap = styled.div`
-  display: flex;
-  flex: 1;
-  width: 100%;
-  background: var(--bg-color);
+	display: flex;
+	flex: 1;
+	width: 100%;
+	background: var(--bg-color);
 `;
 
 const MainWrap = styled.section`
-  display: flex;
-  flex: 1;
-  position: relative;
-  padding: 112px 0 0 0;
-  width: calc(100% - 200px);
+	display: flex;
+	flex: 1;
+	position: relative;
+	padding: 112px 0 0 0;
+	width: calc(100% - 200px);
 
-  .container {
-    width: 100%;
-    max-width: 1040px;
-    padding: 0 32px;
-    margin: 48px auto 48px auto;
-  }
+	.container {
+		width: 100%;
+		max-width: 1040px;
+		padding: 0 32px;
+		margin: 48px auto 48px auto;
+	}
 `;
 const App = () => {
-  return (
-    <Wrap>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Nav />
-        <MainWrap>
-          <Header />
-          {/* 아래 main 안에 각 페이지가 들어갑니다. */}
-          <main className="container">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/" element={<Matching />} />
-              {/*  <Route path="/signup" element={<Signup />} />
-          <Route path={`/:${user-id}`} element={<Profile />} />
-          <Route path="/storyfriend" element={<Story />} />
+	return (
+		<Wrap>
+			<Router basename={process.env.PUBLIC_URL}>
+				<Nav />
+				<MainWrap>
+					<Header />
+					{/* 아래 main 안에 각 페이지가 들어갑니다. */}
+					<main className="container">
+						<Routes>
+							<Route path="/login" element={<Login />} />
+							<Route path="/matchwrite" element={<MatchingWrite />} />
+							<Route path="/" element={<Matching />} />
+							<Route path="/story" element={<Story />} />
+							<Route path="/userid" element={<Profile />} />
+							<Route path="/boardid" element={<MatchingDetail />} />
+							{/*
+          <Route path="/signup" element={<Signup />} />
+          
           <Route path={`/:${user-id}/edit`} element={<Profile />} />
           <Route path={`/:${user-id}/:${board-id}`} element={<StoryDetail />} /> */}
-              {/* <Route path="/storyall" element={<Story />} />
+							{/* 
           <Route path={`/${board - id}`} element={<MatchingDetail />} />
           <Route path="/quit" element={<Quit />} /> */}
-              <Route path="/matchwrite" element={<MatchingWrite />} />
-              <Route path={`/hi`} element={<MatchingDetail />} />
-            </Routes>
-          </main>
-        </MainWrap>
-      </Router>
-    </Wrap>
-  );
+						</Routes>
+					</main>
+				</MainWrap>
+			</Router>
+		</Wrap>
+	);
 };
 
 export default App;

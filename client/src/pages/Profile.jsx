b2d4368c70d0123ccfaa6acac07d508beb3c5832
SUBINSON <fraisecheris@gmail.com>
2023-01-16 21:35:31 +0900

    [FE feat] #79 Connect API with Profile page's left side (temporary member id 98)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import ProfileCard from '../components/ProfileCard';
 import ProfileContent from '../components/ProfileContent';
@@ -7,12 +10,37 @@ const ProfileWrap = styled.div`
 `;
 
 const Profile = () => {
+  const URL = `https://f841-14-63-98-43.jp.ngrok.io`;
+  const [user, setUser] = useState({});
   const isMatch = false;
   const isStory = true;
 
+  useEffect(() => {
+    async function getMember() {
+      await axios
+        .get(`${URL}/api/members/98`, {
+          // ngrok get cors 해결용
+          headers: { 'ngrok-skip-browser-warning': '69420' },
+        })
+        .then((res) => setUser(res.data.data));
+    }
+    getMember();
+  }, []);
+
+  console.log(user);
+
   return (
     <ProfileWrap>
-      <ProfileCard />
+      <ProfileCard
+        iamge={user.iamge}
+        nickname={user.nickname}
+        identifier={'user.identifier'}
+        following={user.following}
+        follower={user.follower}
+        likes={user.likes}
+        games={user.games}
+        introduction={user.introduction}
+      />
       <ProfileContent isMatch={isMatch} isStory={isStory} />
     </ProfileWrap>
   );

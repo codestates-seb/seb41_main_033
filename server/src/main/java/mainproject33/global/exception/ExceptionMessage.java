631dee3eec73edfdb1a81d9eeded75e8bf94ea51
SEB_BE_41_조대희 <111113889+greatshine917@users.noreply.github.com>
2023-01-16 12:45:44 +0900

    [BE feat] #71 멤버 좋아요 구현
    exceptionMessage 추가
    likes api, 엔티티, 비즈니스 로직 추가
    response에 좋아요수가 반영되도록 수정
    팔로우와 좋아요 요청이 권한이 필요하도록 수정

--- server/src/main/java/mainproject33/global/exception/ExceptionMessage.java
+++ server/src/main/java/mainproject33/global/exception/ExceptionMessage.java
@@ -10,10 +10,12 @@ public enum ExceptionMessage {
     //=========member=========//
 
     MEMBER_NOT_FOUND("존재하지 않는 회원 입니다."),
-    MEMBER_UNAUTHORIZED("권한이 없습니다"),
-    MEMBER_EXISTS("회원이 이미 존재합니다"),
+    MEMBER_UNAUTHORIZED("권한이 없습니다."),
+    MEMBER_EXISTS("회원이 이미 존재합니다."),
 
     SELF_FOLLOW_NOT_ALLOWED("자기 자신을 팔로우 할 수 없습니다."),
+
+    SELF_LIKE_NOT_ALLOWED("자기 자신을 좋아요 할 수 없습니다."),
 
     //=========userBoard=========//
     USER_BOARD_NOT_FOUND("존재하지 않는 게시글입니다."),
@@ -25,7 +27,7 @@ public enum ExceptionMessage {
 
     //=========image=========//
     IMAGE_DATA_NOT_FOUND("존재하지 않는 프로필 이미지 데이터입니다."),
-    FILE_NOT_FOUND("존재하지 않는 파일입니다");
+    FILE_NOT_FOUND("존재하지 않는 파일입니다.");
 
 
 

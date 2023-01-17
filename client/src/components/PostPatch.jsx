4d2ce920a60d6fc649672baa7cf07bf041fe86ba
SUBINSON <fraisecheris@gmail.com>
2023-01-17 17:29:19 +0900

    [FE refactor] #82 Use Single Profile Wrap component

--- client/src/components/PostPatch.jsx
+++ client/src/components/PostPatch.jsx
@@ -1,45 +1,20 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import SinglePofileWrap from './SingleProfileWrap';
 
 const CardWrap = styled.div`
   width: 100%;
 `;
 
-const ProfileWrap = styled.div`
-  display: flex;
-  .image_container {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-    overflow: hidden;
-    margin-right: 16px;
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  }
-  .profile_container {
-    width: calc(100% - 96px);
-    margin: auto 0;
-    .nickname {
-      font-size: var(--font-head2-size);
-      color: var(--white);
-    }
-    .identifier {
-      font-size: var(--font-body1-size);
-    }
-  }
-`;
 const ButtonWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 32px;
+  padding-top: 56px;
+  text-align: center;
   button {
     width: 280px;
-  }
-  button:first-child {
     margin-right: 16px;
+  }
+  button:last-child {
+    margin-right: 0;
   }
 `;
 
@@ -53,26 +28,25 @@ const PostPatch = ({
   link2,
   button2,
 }) => {
+  const navigate = useNavigate();
+
   return (
     <CardWrap className="card big">
-      <ProfileWrap>
-        <div className="image_container">
-          <img src={image} alt="프로필 이미지" />
-        </div>
-        <div className="profile_container">
-          <div className="nickname">{nickname}</div>
-          <div className="identifier">{identifier}</div>
-        </div>
-      </ProfileWrap>
+      <SinglePofileWrap
+        imgSize="big"
+        imgSrc={image}
+        name={nickname}
+        subInfo={identifier}
+      />
       <form onSubmit={() => {}}>
         {children}
         <ButtonWrap>
-          <Link to={link1}>
-            <button className="em">{button1}</button>
-          </Link>
-          <Link to={link2}>
-            <button className="normal">{button2}</button>
-          </Link>
+          <button className="em" onClick={() => navigate(`/${link1}`)}>
+            {button1}
+          </button>
+          <button className="normal" onClick={() => navigate(`/${link2}`)}>
+            {button2}
+          </button>
         </ButtonWrap>
       </form>
     </CardWrap>

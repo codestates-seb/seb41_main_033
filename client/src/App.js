aca87511539dfe47d6de6ec815ddc6209be82c3a
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2023-01-17 15:58:16 +0900

    [feat/fe-storyEditView] 스토리 수정 view 구현 (#105)
    * [FE feat] #8 레이아웃 관련 App.js 수정, Header, Nav 컴포넌트 추가
    
    * [FE feat] #8 전역스타일 추가
    
    * [FE feat] #8 index.css 삭제
    
    * [FE feat] #8 폰트 추가
    
    * [FE feat] #21 Nav 컴포넌트 생성
    
    * [FE misc] #21 레이아웃 수정
    
    * [FE design] 전역스타일 추가 및 수정
    
    * [FE misc] #21 Nav 컴포넌트 생성
    
    * [FE feat] #21 필요한 아이콘 svg 추가
    
    * [FE feat] #27 svg아이콘 추가
    
    * [FE feat] #27 Header 컴포넌트 생성
    
    * [FE fix] Nav컴포넌트 레이아웃 수정
    
    * [FE design] 버튼 hover 효과 추가
    
    * [FE misc] 컴포넌트 test 후 수정
    
    * [FE fix] 임시 test 문구 삭제
    
    * [FE feat] #40 유저게시글 목록 개별메시지 컴포넌트 구현
    
    * [FE feat] #40 유저게시글 공통 페이지 컴포넌트 view 구현
    
    * [FE feat] #40 유저게시글 더미데이터 추가
    
    * [FE feat] #40 작성하기 플로트 버튼 컴포넌트 연결
    
    * [FE refactor] #40 불필요한 코드 수정
    
    * [FE misc] #40 data id추가
    
    * [FE misc] #40 Story 라우터 연결
    
    * [FE feat] #40 작성하기 플로트 버튼 컴포넌트 작업
    
    * [FE feat] #40 컴포넌트 라우터 연결
    
    * [FE feat] #56 스토리 상세보기 페이지 연결
    
    * [FE misc] #52 스토리 공통 컴포넌트 분리
    
    * [FE misc] #52 컴포넌트 공통으로 쓰기 위해 props 추가
    
    * [FE feat] #52 하트 아이콘 sprite svg 추가
    
    * [FE feat] #52 프로필 부분 공통 컴포넌트로 분리
    
    * [FE feat] #52 스토리 댓글 컴포넌트 view 구현
    
    * [FE feat] #52 스토리 상세보기 view 구현
    
    * [FE design] #61 input error 스타일 추가
    
    * [FE feat] 회원가입 view 구현
    
    * [FE feat] #61 회원가입 컴포넌트 연결
    
    * [FE feat] #67 이미지 업로드 아이콘 추가
    
    * [FE feat] #67 스토리 작성 페이지 컴포넌트 view 구현
    
    * [FE feat] #67 스토리 작성 페이지 컴포넌트 연결
    
    * [FE fix] activeclassname 기능 수정
    
    * [FE feat] #80 회원가입 유효성 검사 추가, API 연결
    
    * [FE fix] path 앞에 누락된 / 추가
    
    * [FE feat] #103 스토리 작성, 수정 공통 컴포넌트 분리
    
    * [FE feat] #103 스토리 수정 컴포넌트 구현
    
    * [FE feat] #103 스토리 작성 컴포넌트 수정
    
    * [FE feat] #103 스토리 수정 컴포넌트 연결
    
    * [FE fix] #103 오타 수정
    
    * [FE fix] #103 누락 파일 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import styled from 'styled-components';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './style.css';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Matching from './pages/Matching';
-import MatchingDetail from './pages/MatchingDetail';
-import MatchingWrite from './pages/MatchingWrite';
-import Story from './pages/Story';
-import StoryDetail from './pages/StoryDetail';
-import StoryWrite from './pages/StoryWrite';
-import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Quit from './pages/Quit';
-import GameRecommend from './pages/GameRecommend';
+import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./style.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Matching from "./pages/Matching";
+import MatchingDetail from "./pages/MatchingDetail";
+import MatchingWrite from "./pages/MatchingWrite";
+import Story from "./pages/Story";
+import StoryDetail from "./pages/StoryDetail";
+import StoryWrite from "./pages/StoryWrite";
+import StoryEdit from "./pages/StoryEdit";
+import Profile from "./pages/Profile";
+import ProfileEdit from "./pages/ProfileEdit";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Quit from "./pages/Quit";
+import GameRecommend from "./pages/GameRecommend";
 
 const Wrap = styled.div`
 	display: flex;
@@ -38,33 +39,34 @@ const MainWrap = styled.section`
 `;
 
 const App = () => {
-  return (
-    <Wrap>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Nav />
-        <MainWrap>
-          <Header />
-          {/* 아래 main 안에 각 페이지가 들어갑니다. */}
-          <main className="container">
-            <Routes>
-              <Route path="/" element={<Matching />} />
-              <Route path="/:boardid" element={<MatchingDetail />} />
-              <Route path="/matchwrite" element={<MatchingWrite />} />
-              <Route path="/story" element={<Story />} />
-              <Route path="/:userid/:boardid" element={<StoryDetail />} />
-              <Route path="/storywrite" element={<StoryWrite />} />
-              <Route path="/:userid" element={<Profile />} />
-              <Route path="/:userid/edit" element={<ProfileEdit />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/quit" element={<Quit />} />
-              <Route path="/game" element={<GameRecommend />} />
-            </Routes>
-          </main>
-        </MainWrap>
-      </Router>
-    </Wrap>
-  );
+	return (
+		<Wrap>
+			<Router basename={process.env.PUBLIC_URL}>
+				<Nav />
+				<MainWrap>
+					<Header />
+					{/* 아래 main 안에 각 페이지가 들어갑니다. */}
+					<main className="container">
+						<Routes>
+							<Route path="/" element={<Matching />} />
+							<Route path="/:boardid" element={<MatchingDetail />} />
+							<Route path="/matchwrite" element={<MatchingWrite />} />
+							<Route path="/story" element={<Story />} />
+							<Route path="/:userid/:boardid" element={<StoryDetail />} />
+							<Route path="/:userid/:boardid/edit" element={<StoryEdit />} />
+							<Route path="/storywrite" element={<StoryWrite />} />
+							<Route path="/:userid" element={<Profile />} />
+							<Route path="/:userid/edit" element={<ProfileEdit />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/signup" element={<Signup />} />
+							<Route path="/quit" element={<Quit />} />
+							<Route path="/game" element={<GameRecommend />} />
+						</Routes>
+					</main>
+				</MainWrap>
+			</Router>
+		</Wrap>
+	);
 };
 
 export default App;

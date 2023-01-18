247bc6ec4f78876bf61b03f64b7e83bf635ca49e
DreamChaserDeekay <deekayshin@gmail.com>
2023-01-18 21:05:18 +0900

    [BE feat] #134 매칭 게시글 block 필터링 1. 매칭 게시글 Block 기능 구현    - 사용자가 회원일 경우 : block 회원이 올린 매칭 게시글은 안 보임.    - 사용자가 비회원일 경우 : 다 보임    - 목록 GET, 검색 GET 모두 테스트 완료    - 단일 GET 시 차단 회원이 올린 글이라면, ExceptionMessage 만 return    - ExceptionMessage 추가 (MATCH_BOARD_BLOCKED) 2. PageableDefault 로 설정한 Direction 값이 먹지 않아, repository @Query 문으로 해결 (findAll() override, findByKeyword 쿼리문 수정)    - 원래 pageable 값을 repository 에서 parameter 로 받았으나, 받지 않는 것으로 수정 이후 목록이 ASC 순으로 나온 것으로 보아,    pageable 의 설정값은 repository 에서 찾을 때만 적용되는 값인 것으로 추정..

--- server/src/main/java/mainproject33/domain/member/repository/BlockRepository.java
+++ server/src/main/java/mainproject33/domain/member/repository/BlockRepository.java
@@ -4,11 +4,13 @@ import mainproject33.domain.member.entity.Block;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BlockRepository extends JpaRepository<Block, Long> {
 
     @Query(value = "SELECT * FROM BLOCK WHERE BLOCKER_ID = :blockerId AND BLOCKED_ID = :blockedId", nativeQuery = true)
     Optional<Block> findByBlock(Long blockerId, Long blockedId);
-
+    @Query(value = "SELECT * FROM BLOCK WHERE BLOCKER_ID = :blockerId", nativeQuery = true)
+    List<Block> findByBlockerId(Long blockerId);
 }

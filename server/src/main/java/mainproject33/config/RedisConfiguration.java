243e64b4f8d4f9db4d8861f442fc32bdb5abc4b0
SEB_BE_41_조대희 <111113889+greatshine917@users.noreply.github.com>
2023-01-18 23:43:16 +0900

    [BE refactor] #132 로그아웃 구현
    redis 를 이용하여 로그아웃 시 토큰을 만료시간을 충족하기 전에 삭제할 수 있도록
    구현

--- server/src/main/java/mainproject33/config/RedisConfiguration.java
+++ server/src/main/java/mainproject33/config/RedisConfiguration.java
@@ -18,6 +18,10 @@ public class RedisConfiguration {
 
     private final RedisProperties redisProperties;
 
+    /**
+     * RedisConnectionFactory = redis 에 접근하기 위한 redis 저장소와 연결할 때 필요
+     * RedisStandaloneConfiguration = single node 에 redis 를 연결하기 위한 설정 정보를 가지고 있는 기본 클래스
+     */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisConfiguration =
@@ -28,6 +32,11 @@ public class RedisConfiguration {
         return new LettuceConnectionFactory(redisConfiguration);
     }
 
+    /**
+     *  redisTemplate 은 redis 서버에 redis 커맨드 수행을 하기 위한 것을 제공
+     *  setKeySerializer(), setValueSerializer() 설정해주는 이유는
+     *  RedisTemplate 를 사용할 때 Spring - Redis 간 데이터 직렬화, 역직렬화 시 사용하는 방식이 Jdk 직렬화 방식이기 때문
+     */
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();

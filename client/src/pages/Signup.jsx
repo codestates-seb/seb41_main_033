cc686ffa6eaac46a89558a49fb44a25b557029cb
SEB_FE_41_김소라 <110882351+kimsorara@users.noreply.github.com>
2023-01-18 13:13:29 +0900

    Feat/fe frefactor (#119)
    * [FE feat] #100 매칭하기 수정페이지 View
    
    * [FE misc]간단한 코드수정
    
    * [FE refactor] 간단한코드수정

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -5,38 +5,38 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 const Wrap = styled.div`
-	width: var(--col-6);
-	margin: 0 auto;
+  width: var(--col-6);
+  margin: 0 auto;
 
-	button.em {
-		width: 100%;
-		margin-top: 24px;
-	}
+  button.em {
+    width: 100%;
+    margin-top: 24px;
+  }
 `;
 
 const Title = styled.h3`
-	width: 100%;
-	text-align: center;
-	margin-bottom: 24px;
-	font-size: var(--font-head2-size);
-	font-weight: var(--font-weight-medium);
+  width: 100%;
+  text-align: center;
+  margin-bottom: 24px;
+  font-size: var(--font-head2-size);
+  font-weight: var(--font-weight-medium);
 `;
 
 const InputWrap = styled.div`
-	margin-bottom: 24px;
+  margin-bottom: 24px;
 
-	label {
-		width: 100%;
-		margin-bottom: 16px;
-	}
-	input[type="text"],
-	input[type="password"] {
-		width: 100%;
-	}
+  label {
+    width: 100%;
+    margin-bottom: 16px;
+  }
+  input[type="text"],
+  input[type="password"] {
+    width: 100%;
+  }
 
-	.error_caption {
-		padding: 0 16px;
-	}
+  .error_caption {
+    padding: 0 16px;
+  }
 `;
 
 const Signup = () => {
@@ -79,30 +79,59 @@ const Signup = () => {
 			.catch((err) => alert("회원가입에 실패하였습니다."));
 	};
 
-	return (
-		<Wrap className="card big">
-			<Title>회원가입</Title>
-			<form onSubmit={(e) => e.preventDefault()}>
-				<InputWrap>
-					<label>아이디</label>
-					<input type="text" placeholder="4~16자의 영문, 숫자의 아이디를 입력하세요" onChange={(e) => handleInputValue("identifier", e)} className={isError.identifier ? "error" : ""} />
-					{isError.identifier ? <div className="error_caption">아이디는 4~16자의 영문, 숫자만 가능합니다.</div> : null}
-				</InputWrap>
-				<InputWrap>
-					<label>비밀번호</label>
-					<input type="password" placeholder="비밀번호를 입력하세요" onChange={(e) => handleInputValue("password", e)} className={isError.password ? "error" : ""} autoComplete="on" />
-					{isError.password ? <div className="error_caption">비밀번호는 8~20자의 영문, 숫자, 특수문자(~, !, @)가 모두 포함되어야 합니다.</div> : null}
-				</InputWrap>
-				<InputWrap>
-					<label>닉네임</label>
-					<input type="text" placeholder="2~8자의 영문, 한글, 숫자로 된 닉네임을 입력하세요" onChange={(e) => handleInputValue("nickname", e)} className={isError.nickname ? "error" : ""} />
-					{isError.nickname ? <div className="error_caption">닉네임은 2~8자의 영문, 한글, 숫자만 가능합니다.</div> : null}
-				</InputWrap>
-				<button className="em" onClick={handleSignup}>
-					회원가입
-				</button>
-			</form>
-		</Wrap>
-	);
+  return (
+    <Wrap className="card big">
+      <Title>회원가입</Title>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <InputWrap>
+          <label>아이디</label>
+          <input
+            type="text"
+            placeholder="4~16자의 영문, 숫자의 아이디를 입력하세요"
+            onChange={(e) => handleInputValue("identifier", e)}
+            className={isError.identifier ? "error" : ""}
+          />
+          {isError.identifier ? (
+            <div className="error_caption">
+              아이디는 4~16자의 영문, 숫자만 가능합니다.
+            </div>
+          ) : null}
+        </InputWrap>
+        <InputWrap>
+          <label>비밀번호</label>
+          <input
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+            onChange={(e) => handleInputValue("password", e)}
+            className={isError.password ? "error" : ""}
+            autoComplete="on"
+          />
+          {isError.password ? (
+            <div className="error_caption">
+              비밀번호는 8~20자의 영문, 숫자, 특수문자(~, !, @)가 모두
+              포함되어야 합니다.
+            </div>
+          ) : null}
+        </InputWrap>
+        <InputWrap>
+          <label>닉네임</label>
+          <input
+            type="text"
+            placeholder="2~8자의 영문, 한글, 숫자로 된 닉네임을 입력하세요"
+            onChange={(e) => handleInputValue("nickname", e)}
+            className={isError.nickname ? "error" : ""}
+          />
+          {isError.nickname ? (
+            <div className="error_caption">
+              닉네임은 2~8자의 영문, 한글, 숫자만 가능합니다.
+            </div>
+          ) : null}
+        </InputWrap>
+        <button className="em" onClick={handleSignup}>
+          회원가입
+        </button>
+      </form>
+    </Wrap>
+  );
 };
 export default Signup;

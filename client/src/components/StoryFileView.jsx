cc16a337f36659c41fede6305538eecd4e5e72a7
shinnh2 <nahyeon.shin9@gmail.com>
2023-01-20 02:12:53 +0900

    [FE feat] #140 스토리 이미지, 비디오 렌더링 공통 컴포넌트 분리

---
+++ client/src/components/StoryFileView.jsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+const ImgArea = styled.div`
+	width: ${(props) => (props.size === "big" ? "100%" : "var(--col-3)")};
+	height: ${(props) => (props.size === "big" ? "auto" : "var(--col-3)")};
+	${(props) => (props.size === "big" ? "margin-bottom:24px;" : "")}
+	${(props) => (props.size === "big" ? "" : "border-radius:var(--border-radius-lg);")}
+	background: ${(props) => (props.size === "big" ? "none" : "var(--grey)")};
+	overflow: hidden;
+
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+`;
+
+const VideoArea = styled.div`
+	width: ${(props) => (props.size === "big" ? "100%" : "var(--col-3)")};
+	height: ${(props) => (props.size === "big" ? "auto" : "var(--col-3)")};
+	${(props) => (props.size === "big" ? "margin-bottom:24px;" : "")}
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	overflow: hidden;
+
+	video {
+		width: 100%;
+		height: 100%;
+	}
+`;
+
+const StoryFileView = ({ size, fileName, contentType }) => {
+	//파일 type 판별
+	let type = "";
+	if (contentType) type = contentType.split("/")[0];
+	return (
+		<>
+			{fileName && type === "image" ? (
+				<ImgArea size={size}>
+					<img src={fileName} />
+				</ImgArea>
+			) : null}
+			{fileName && type === "video" ? (
+				<VideoArea size={size}>
+					<video controls>
+						<source src={fileName} type={contentType} />
+					</video>
+				</VideoArea>
+			) : null}
+		</>
+	);
+};
+export default StoryFileView;

4b5988695353b40d9d09d894e6461eb7e5190039
SEB_BE_41_조대희 <111113889+greatshine917@users.noreply.github.com>
2023-01-19 11:55:27 +0900

    [Feat/be blocklist] 차단 목록 구현 (#147)
    [BE refactor] #141 차단 목록 구현
    
    회원이 자신이 차단한 회원의 목록을 확인할 수 있도록 했다
    차단을 하면 팔로우와 좋아요가 취소되게끔 변경,
    차단이 되어 있다면 팔로우와 좋아요를 취소할 수 없게 로직 구성
    그 와 관련된 ExceptionMessage 추가

--- server/src/main/java/mainproject33/domain/member/repository/BlockRepository.java
+++ server/src/main/java/mainproject33/domain/member/repository/BlockRepository.java
@@ -12,6 +12,9 @@ public interface BlockRepository extends JpaRepository<Block, Long> {
     @Query(value = "SELECT * FROM BLOCK WHERE BLOCKER_ID = :blockerId AND BLOCKED_ID = :blockedId", nativeQuery = true)
     Optional<Block> findByBlock(Long blockerId, Long blockedId);
 
+    @Query(value = "SELECT * FROM BLOCK WHERE BLOCKER_ID = :blockerId", nativeQuery = true)
+    List<Block> findByBlockList(Long blockerId);
+
     @Query(value = "select blocked_id from block where blocker_id = :blockerId", nativeQuery = true)
     List<Long> findBlockedIdByBlockerId(Long blockerId);
 

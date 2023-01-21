40a8a11873b4bf4787b8d1313461adfbd4533e1f
shinnh2 <nahyeon.shin9@gmail.com>
2023-01-21 16:42:07 +0900

    [FE fix] #121 병합 충돌 수정

--- client/src/pages/Story.jsx
+++ client/src/pages/Story.jsx
@@ -1,69 +1,70 @@
-import styled from 'styled-components';
-import SearchBar from './../components/SearchBar';
-import StorySingle from '../components/StorySingle';
-import WriteFloatButton from '../components/WriteFloatButton';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { API_URL } from '../data/apiUrl';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import styled from "styled-components";
+import SearchBar from "./../components/SearchBar";
+import StorySingle from "../components/StorySingle";
+import WriteFloatButton from "../components/WriteFloatButton";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "../data/apiUrl";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const TitleWrap = styled.div`
-  display: flex;
-  margin-bottom: 32px;
-  h3 {
-    margin-right: 24px;
-    letter-spacing: -0.5px;
-    font-size: var(--font-head2-size);
-  }
-  h3.active {
-    font-weight: var(--font-weight-medium);
-    color: var(--primary-color);
-  }
+	display: flex;
+	margin-bottom: 32px;
+	h3 {
+		margin-right: 24px;
+		letter-spacing: -0.5px;
+		font-size: var(--font-head2-size);
+	}
+	h3.active {
+		font-weight: var(--font-weight-medium);
+		color: var(--primary-color);
+	}
 `;
 
 const StoryBoardWrap = styled.div`
-  margin-top: 48px;
-  > div {
-    margin-bottom: 24px;
-  }
+	margin-top: 48px;
+	> div {
+		margin-bottom: 24px;
+	}
 `;
 
 const Story = () => {
-  const { accessToken } = useSelector((state) => state.islogin.login);
-  const navigate = useNavigate();
-  const [storyData, setStoryData] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API_URL}/api/boards?page=1`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .then((res) => {
-        setStoryData(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [accessToken]);
-  const handleWriteFBtnOnClick = () => {
-    navigate(`/storywrite`);
-  };
-  return (
-    <div>
-      <TitleWrap>
-        <h3 className="active">모두의 스토리</h3>
-        <h3>친구의 스토리</h3>
-      </TitleWrap>
-      <SearchBar />
-      <StoryBoardWrap>
-        {storyData?.map((el) => {
-          return <StorySingle key={el.id} data={el} />;
-        })}
-      </StoryBoardWrap>
-      <WriteFloatButton click={handleWriteFBtnOnClick} />
-    </div>
-  );
+	const { accessToken } = useSelector((state) => state.islogin.login);
+	const navigate = useNavigate();
+	const [storyData, setStoryData] = useState([]);
+	useEffect(() => {
+		axios
+			.get(`${API_URL}/api/boards?page=1`, {
+				// headers: {
+				// 	// "ngrok-skip-browser-warning": "69420",
+				// 	Authorization: `Bearer ${ACCESS_TOKEN}`,
+				// },
+			})
+			.then((res) => {
+				setStoryData(res.data.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+	const handleWriteFBtnOnClick = () => {
+		navigate(`/storywrite`);
+	};
+	return (
+		<div>
+			<TitleWrap>
+				<h3 className="active">모두의 스토리</h3>
+				<h3>친구의 스토리</h3>
+			</TitleWrap>
+			<SearchBar />
+			<StoryBoardWrap>
+				{storyData?.map((el) => {
+					return <StorySingle key={el.id} data={el} />;
+				})}
+			</StoryBoardWrap>
+			<WriteFloatButton click={handleWriteFBtnOnClick} />
+		</div>
+	);
 };
 export default Story;
--- client/src/pages/StoryEdit.jsx
+++ client/src/pages/StoryEdit.jsx
@@ -1,6 +1,54 @@
-import StoryInputArea from "../components/StoryInputArea";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { API_URL } from "../data/apiUrl";
+import axios from "axios";
+import PostPatch from "../components/PostPatch";
+
+const TextArea = styled.div`
+	margin: 24px 0 16px 0;
+`;
 
 const StoryEdit = () => {
-	return <StoryInputArea page="edit" />;
+	const [userData, setUserData] = useState({});
+	const [content, setContent] = useState("");
+	useEffect(() => {
+		axios
+			.get(`${API_URL}/api/members/${localStorage.getItem("memberId")}`)
+			.then((res) => {
+				setUserData(res.data.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	//입력
+	const handleContentOnChange = (e) => {
+		setContent(e.currentTarget.value);
+	};
+	//제출
+	const handleStoryEditSubmit = () => {
+		console.log("스토리 수정 버튼 클릭");
+	};
+
+	return (
+		<PostPatch
+			image={userData.profileImage}
+			nickname={userData.nickname}
+			identifier={userData.identifier}
+			button1="수정완료"
+			button2="취소"
+			link={-1}
+			handleSubmit={handleStoryEditSubmit}
+		>
+			<TextArea>
+				<label>내용</label>
+				<textarea
+					placeholder="스토리 내용만 수정만 가능합니다"
+					onChange={(e) => handleContentOnChange(e)}
+				></textarea>
+			</TextArea>
+		</PostPatch>
+	);
 };
 export default StoryEdit;

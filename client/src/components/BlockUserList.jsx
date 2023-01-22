ecff6faac26f8722a98bf16dba875ebf5ee4240c
SUBINSON <fraisecheris@gmail.com>
2023-01-22 18:49:20 +0900

    [FE design] #133 Default style of Block List page

--- client/src/components/BlockUserList.jsx
+++ client/src/components/BlockUserList.jsx
@@ -1,5 +1,51 @@
 import styled from 'styled-components';
+import SingleProfileWrap from './SingleProfileWrap';
 
-const BlockUserList = () => {};
+const ListWrap = styled.div`
+  li {
+    display: flex;
+    justify-content: space-between;
+    padding: 16px 48px;
+    border-top: 1px solid var(--border-color);
+    .profile_container {
+      margin-bottom: 0px;
+    }
+    .unblock_btn {
+      color: var(--error-color);
+    }
+  }
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 32px;
+  button {
+    width: 280px;
+  }
+`;
+
+const BlockUserList = () => {
+  return (
+    <>
+      <ListWrap>
+        <ul>
+          <li>
+            <SingleProfileWrap
+              className="profile_container"
+              imgSrc={''}
+              name={'닉네임'}
+              subInfo={'아이디'}
+            />
+            <button className="unblock_btn">차단해제</button>
+          </li>
+        </ul>
+      </ListWrap>
+      <ButtonWrap>
+        <button className="em">편집 완료</button>
+      </ButtonWrap>
+    </>
+  );
+};
 
 export default BlockUserList;
--- client/src/pages/BlockList.jsx
+++ client/src/pages/BlockList.jsx
@@ -1,7 +1,27 @@
 import styled from 'styled-components';
+import BlockUserList from '../components/BlockUserList';
+
+const BlockWrap = styled.div`
+  width: 100%;
+  padding: 48px 0;
+`;
+
+const TitleWrap = styled.div`
+  padding: 0 48px 48px 48px;
+  .title {
+    font-size: var(--font-head2-size);
+  }
+`;
 
 const BlockList = () => {
-  return <></>;
+  return (
+    <BlockWrap className="card">
+      <TitleWrap>
+        <div className="title">차단한 유저 목록</div>
+      </TitleWrap>
+      <BlockUserList />
+    </BlockWrap>
+  );
 };
 
 export default BlockList;

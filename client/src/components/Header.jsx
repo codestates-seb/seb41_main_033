7e4ba083f3f423922797a3d8a78c08ef254da01d
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2023-01-25 20:17:59 +0900

    [feat/fe-storyRefactor] 스토리 리팩토링, 부족한 기능 추가 (#206)
    * [FE fix] #191 병합충돌 해결
    
    * [FE fix] #191 병합 충돌 해결
    
    * [FE misc] #191 스토리 작성하기 path 수정
    
    * [FE refactor] #191 무한 스크롤 전페 페이지 수만큼 요청 제한
    
    * [FE fix] #191 댓글 무한요청 수정
    
    * [FE feat] #191 스토리 좋아요 기능 구현
    
    * [FE fix] #191 스토리 목록 페이지 번호 버그 수정
    
    * [FE misc] #191 콘솔 제거
    
    * [FE misc] #191 스토리 기능 고도화
    
    * [FE refactor] #191 스토리 수정 삭제시 바로 보이게 코드 수정
    
    * [FE fix] #191 좋아요 기능 수정
    
    * [FE refactor] #191 좋아요 기능 수정
    
    * [FE feat] #191 댓글 좋아요 기능 추가
    
    * [FE misc] #191 스토리 목록 없을 때 문구 추가, 스토리 작성시 로그인 후 이용가능 하게 기능 추가
    
    * [FE fix] #191 검색바 커서 부분 잘 나오게 CSS 수정
    
    * [FE fix] #191 병합 충돌 수정
    
    * [FE feat] #191 검색 기능 구현

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,152 +1,151 @@
-import styled from 'styled-components';
-import { ReactComponent as ProfileImg } from './../assets/defaultImg.svg';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { API_URL } from '../data/apiUrl';
-import { logout } from '../redux/slice/loginstate';
-import { userInfo } from '../redux/slice/userInfo';
+import styled from "styled-components";
+import { ReactComponent as ProfileImg } from "./../assets/defaultImg.svg";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { API_URL } from "../data/apiUrl";
+import { logout } from "../redux/slice/loginstate";
+import { userInfo } from "../redux/slice/userInfo";
 const HeaderWrap = styled.header`
-  position: absolute;
-  left: 0;
-  top: 0;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  width: 100%;
-  height: 112px;
-  padding: 0 48px;
+	position: absolute;
+	left: 0;
+	top: 0;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	width: 100%;
+	height: 112px;
+	padding: 0 48px;
 `;
 
 const ProfileWrap = styled.div`
-  a {
-    flex: none;
-    display: flex;
-    align-items: center;
-    margin-left: 32px;
-  }
-  .alert {
-    display: block;
-    width: 6px;
-    height: 6px;
-    margin-right: 6px;
-    border-radius: 6px;
-    background: var(--primary-color);
-    font-size: 0;
-    overflow: hidden;
-  }
-  .user_nickname {
-    margin-right: 12px;
-    font-size: var(--font-caption-size);
-    color: var(--font-color);
-  }
-  .user_img {
-    width: 48px;
-    height: 48px;
-    border-radius: 50%;
-    overflow: hidden;
-    img {
-      width: 100%;
-      height: 100%;
-    }
-  }
+	a {
+		flex: none;
+		display: flex;
+		align-items: center;
+		margin-left: 32px;
+	}
+	.alert {
+		display: block;
+		width: 6px;
+		height: 6px;
+		margin-right: 6px;
+		border-radius: 6px;
+		background: var(--primary-color);
+		font-size: 0;
+		overflow: hidden;
+	}
+	.user_nickname {
+		margin-right: 12px;
+		font-size: var(--font-caption-size);
+		color: var(--font-color);
+	}
+	.user_img {
+		width: 48px;
+		height: 48px;
+		border-radius: 50%;
+		overflow: hidden;
+		img {
+			width: 100%;
+			height: 100%;
+		}
+	}
 `;
 
 const BtnWrap = styled.div`
-  flex: none;
-  display: flex;
-  margin-left: 48px;
-  a {
-    display: block;
-    padding: 6px 16px;
-    font-size: var(--font-body2-size);
-    font-weight: var(--font-weight-md);
-    color: var(--strong-color);
-  }
-  a.active {
-    color: var(--primary-color);
-  }
-  button {
-    border: 1px solid var(--border-color);
-    border-radius: 8px;
-    font-size: var(--font-caption-size);
-  }
+	flex: none;
+	display: flex;
+	margin-left: 48px;
+	a {
+		display: block;
+		padding: 6px 16px;
+		font-size: var(--font-body2-size);
+		font-weight: var(--font-weight-md);
+		color: var(--strong-color);
+	}
+	a.active {
+		color: var(--primary-color);
+	}
+	button {
+		border: 1px solid var(--border-color);
+		border-radius: 8px;
+		font-size: var(--font-caption-size);
+	}
 `;
 
 const Header = () => {
-  const loginInfo = useSelector((state) => state.islogin.login);
-  const userInform = useSelector((state) => state.userInfo.userInfo);
-  const [nickname, setNickname] = useState(null);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+	const loginInfo = useSelector((state) => state.islogin.login);
+	const [user, setUser] = useState({});
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (loginInfo?.isLogin) {
-      axios.get(`${API_URL}/api/members/${loginInfo?.memberId}`).then((res) => {
-        setNickname(res.data.data.nickname);
-        dispatch(userInfo(res.data.data));
-      });
-    }
-  }, [loginInfo?.isLogin, loginInfo?.memberId, dispatch]);
+	useEffect(() => {
+		if (loginInfo?.isLogin) {
+			axios.get(`${API_URL}/api/members/${loginInfo?.memberId}`).then((res) => {
+				setUser(res.data.data);
+				dispatch(userInfo(res.data.data));
+			});
+		}
+	}, [loginInfo?.isLogin, loginInfo?.memberId]);
 
-  const handleLogout = () => {
-    axios
-      .post(
-        `${API_URL}/api/members/logout`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${loginInfo.accessToken}`,
-          },
-        }
-      )
-      .then(() => {
-        localStorage.clear();
-        dispatch(logout({ accessToken: null, memberId: null, isLogin: false }));
-        navigate('/');
-      });
-  };
+	const handleLogout = () => {
+		axios
+			.post(
+				`${API_URL}/api/members/logout`,
+				{},
+				{
+					headers: {
+						Authorization: `Bearer ${loginInfo.accessToken}`,
+					},
+				}
+			)
+			.then(() => {
+				localStorage.clear();
+				dispatch(logout({ accessToken: null, memberId: null, isLogin: false }));
+				navigate("/");
+			});
+	};
 
-  return (
-    <HeaderWrap>
-      {userInform && loginInfo?.isLogin ? (
-        <>
-          <ProfileWrap>
-            <a href={`/profile/`}>
-              <span className="alert">알림</span>
-              <span className="user_nickname">{nickname}</span>
-              <div className="user_img">
-                {userInform.profileImage ? (
-                  <img src={userInform.profileImage} alt="프로필이미지" />
-                ) : (
-                  <ProfileImg />
-                )}
-              </div>
-            </a>
-          </ProfileWrap>
-          <BtnWrap>
-            <button onClick={handleLogout}>로그아웃</button>
-          </BtnWrap>
-        </>
-      ) : (
-        <BtnWrap>
-          <NavLink
-            to="/signup"
-            className={({ isActive }) => (isActive ? 'active' : '')}
-          >
-            회원가입
-          </NavLink>
-          <NavLink
-            to="/login"
-            className={({ isActive }) => (isActive ? 'active' : '')}
-          >
-            로그인
-          </NavLink>
-        </BtnWrap>
-      )}
-    </HeaderWrap>
-  );
+	return (
+		<HeaderWrap>
+			{user && loginInfo?.isLogin ? (
+				<>
+					<ProfileWrap>
+						<a href={`/profile/${loginInfo?.memberId}`}>
+							<span className="alert">알림</span>
+							<span className="user_nickname">{user.nickname}</span>
+							<div className="user_img">
+								{user.profileImage ? (
+									<img src={user.profileImage} alt="프로필이미지" />
+								) : (
+									<ProfileImg />
+								)}
+							</div>
+						</a>
+					</ProfileWrap>
+					<BtnWrap>
+						<button onClick={handleLogout}>로그아웃</button>
+					</BtnWrap>
+				</>
+			) : (
+				<BtnWrap>
+					<NavLink
+						to="/signup"
+						className={({ isActive }) => (isActive ? "active" : "")}
+					>
+						회원가입
+					</NavLink>
+					<NavLink
+						to="/login"
+						className={({ isActive }) => (isActive ? "active" : "")}
+					>
+						로그인
+					</NavLink>
+				</BtnWrap>
+			)}
+		</HeaderWrap>
+	);
 };
 
 export default Header;

aabae47c75d1799a06c982d3a3e47cb7e36f81d2
SUBINSON <fraisecheris@gmail.com>
2023-01-26 16:16:00 +0900

    [FE misc] #205 Modify route

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import styled from "styled-components";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./style.css";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Matching from "./pages/Matching";
-import MatchingDetail from "./pages/MatchingDetail";
-import MatchingWrite from "./pages/MatchingWrite";
-import MatchingEdit from "./pages/MatchingEdit";
-import Story from "./pages/Story";
-import StoryDetail from "./pages/StoryDetail";
-import StoryWrite from "./pages/StoryWrite";
-import StoryEdit from "./pages/StoryEdit";
-import Profile from "./pages/Profile";
-import ProfileEdit from "./pages/ProfileEdit";
-import BlockList from "./pages/BlockList";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Quit from "./pages/Quit";
-import GameRecommend from "./pages/GameRecommend";
-import Readme from "./pages/Readme";
+import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './style.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Matching from './pages/Matching';
+import MatchingDetail from './pages/MatchingDetail';
+import MatchingWrite from './pages/MatchingWrite';
+import MatchingEdit from './pages/MatchingEdit';
+import Story from './pages/Story';
+import StoryDetail from './pages/StoryDetail';
+import StoryWrite from './pages/StoryWrite';
+import StoryEdit from './pages/StoryEdit';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import BlockList from './pages/BlockList';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Quit from './pages/Quit';
+import GameRecommend from './pages/GameRecommend';
+import Readme from './pages/Readme';
 
 const Wrap = styled.div`
   display: flex;
@@ -51,6 +51,7 @@ const App = () => {
           {/* 아래 main 안에 각 페이지가 들어갑니다. */}
           <main className="container">
             <Routes>
+              <Route path="/" element={<Matching />} />
               <Route path="/match" element={<Matching />} />
               <Route
                 path="/match/:boardid/detail"
@@ -72,7 +73,7 @@ const App = () => {
               <Route path="/signup" element={<Signup />} />
               <Route path="/quit" element={<Quit />} />
               <Route path="/game" element={<GameRecommend />} />
-              <Route path="/" element={<Readme />} />
+              <Route path="/curtaincall" element={<Readme />} />
             </Routes>
           </main>
         </MainWrap>

0df45e06ae5b6a41f476347ecbb45cfc67818c73
SUBINSON <fraisecheris@gmail.com>
2023-01-26 17:04:53 +0900

    [FE feat] #205 Header logout Popup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,96 +1,106 @@
-import styled from "styled-components";
-import { ReactComponent as ProfileImg } from "./../assets/defaultImg.svg";
-import { NavLink, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { API_URL } from "../data/apiUrl";
-import { logout } from "../redux/slice/loginstate";
-import { userInfo } from "../redux/slice/userInfo";
-import { login } from "../redux/slice/loginstate";
+import styled from 'styled-components';
+import { ReactComponent as ProfileImg } from './../assets/defaultImg.svg';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_URL } from '../data/apiUrl';
+import { logout } from '../redux/slice/loginstate';
+import { userInfo } from '../redux/slice/userInfo';
+import { login } from '../redux/slice/loginstate';
+import Popup from './Popup';
 
 const HeaderWrap = styled.header`
-	position: absolute;
-	left: 0;
-	top: 0;
-	display: flex;
-	align-items: center;
-	justify-content: flex-end;
-	width: 100%;
-	height: 112px;
-	padding: 0 48px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  height: 112px;
+  padding: 0 48px;
 `;
 
 const ProfileWrap = styled.div`
-	a {
-		flex: none;
-		display: flex;
-		align-items: center;
-		margin-left: 32px;
-	}
-	.alert {
-		display: block;
-		width: 6px;
-		height: 6px;
-		margin-right: 6px;
-		border-radius: 6px;
-		background: var(--primary-color);
-		font-size: 0;
-		overflow: hidden;
-	}
-	.user_nickname {
-		margin-right: 12px;
-		font-size: var(--font-caption-size);
-		color: var(--font-color);
-	}
-	.user_img {
-		width: 48px;
-		height: 48px;
-		border-radius: 50%;
-		overflow: hidden;
-		img {
-			width: 100%;
-			height: 100%;
-		}
-	}
+  a {
+    flex: none;
+    display: flex;
+    align-items: center;
+    margin-left: 32px;
+  }
+  .alert {
+    display: block;
+    width: 6px;
+    height: 6px;
+    margin-right: 6px;
+    border-radius: 6px;
+    background: var(--primary-color);
+    font-size: 0;
+    overflow: hidden;
+  }
+  .user_nickname {
+    margin-right: 12px;
+    font-size: var(--font-caption-size);
+    color: var(--font-color);
+  }
+  .user_img {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
 `;
 
 const BtnWrap = styled.div`
-	flex: none;
-	display: flex;
-	margin-left: 48px;
-	a {
-		display: block;
-		padding: 6px 16px;
-		font-size: var(--font-body2-size);
-		font-weight: var(--font-weight-md);
-		color: var(--strong-color);
-	}
-	a.active {
-		color: var(--primary-color);
-	}
-	button {
-		border: 1px solid var(--border-color);
-		border-radius: 8px;
-		font-size: var(--font-caption-size);
-	}
+  flex: none;
+  display: flex;
+  margin-left: 48px;
+  a {
+    display: block;
+    padding: 6px 16px;
+    font-size: var(--font-body2-size);
+    font-weight: var(--font-weight-md);
+    color: var(--strong-color);
+  }
+  a.active {
+    color: var(--primary-color);
+  }
+  button {
+    border: 1px solid var(--border-color);
+    border-radius: 8px;
+    font-size: var(--font-caption-size);
+  }
 `;
 
 const Header = () => {
-	const loginInfo = useSelector((state) => state.islogin.login);
-	const [user, setUser] = useState({});
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  const loginInfo = useSelector((state) => state.islogin.login);
+  const userInform = useSelector((state) => state.userInfo.userInfo);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		if (loginInfo?.isLogin) {
-			axios.get(`${API_URL}/api/members/${loginInfo?.memberId}`).then((res) => {
-				setUser(res.data.data);
-				dispatch(userInfo(res.data.data));
-			});
-		}
-	}, [loginInfo?.isLogin, loginInfo?.memberId]);
+  useEffect(() => {
+    if (loginInfo?.isLogin) {
+      axios.get(`${API_URL}/api/members/${loginInfo?.memberId}`).then((res) => {
+        dispatch(userInfo(res.data.data));
+      });
+    }
+  }, [loginInfo?.isLogin, loginInfo?.memberId, dispatch]);
 
+  const handlePopup = () => {
+    setIsOpen((prev) => !prev);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const handleCancel = () => {
+    setIsOpen((prev) => !prev);
+    document.body.style.overflow = 'unset';
+  };
 
   const handleLogout = () => {
     axios
@@ -105,8 +115,10 @@ const Header = () => {
       )
       .then(() => {
         localStorage.clear();
+        setIsOpen((prev) => !prev);
+        document.body.style.overflow = 'unset';
         dispatch(logout({ accessToken: null, memberId: null, isLogin: false }));
-        navigate("/");
+        navigate('/');
       });
   };
 
@@ -114,7 +126,7 @@ const Header = () => {
     if (loginInfo?.isLogin && Date.now() >= loginInfo?.expire) {
       const memberId = loginInfo.memberId;
       const expire = Date.now() + 1000 * 60 * 20;
-      console.log(loginInfo.refreshtoken);
+
       axios
         .get(
           `${API_URL}/api/members/${loginInfo?.memberId}`,
@@ -129,7 +141,6 @@ const Header = () => {
         .then((res) => {
           const refreshtoken = res.headers.refreshtoken;
           const accessToken = res.headers.authorization;
-          setNickname(res.data.data.nickname);
           dispatch(userInfo(res.data.data));
           dispatch(
             login({
@@ -140,6 +151,7 @@ const Header = () => {
               refreshtoken,
             })
           );
+          window.location.reload();
         })
         .catch((err) => console.log(err));
     }
@@ -149,9 +161,9 @@ const Header = () => {
       {userInform && loginInfo?.isLogin ? (
         <>
           <ProfileWrap>
-            <a href={`/profile/`}>
+            <a href={`/profile/${loginInfo?.memberId}`}>
               <span className="alert">알림</span>
-              <span className="user_nickname">{nickname}</span>
+              <span className="user_nickname">{userInform.nickname}</span>
               <div className="user_img">
                 {userInform.profileImage ? (
                   <img src={userInform.profileImage} alt="프로필이미지" />
@@ -162,20 +174,30 @@ const Header = () => {
             </a>
           </ProfileWrap>
           <BtnWrap>
-            <button onClick={handleLogout}>로그아웃</button>
+            <button onClick={handlePopup}>로그아웃</button>
           </BtnWrap>
+          <Popup
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            title="로그아웃"
+            content="로그아웃 하시겠습니까?"
+            button1="로그아웃"
+            button2="취소"
+            handleBtn1={handleLogout}
+            handleBtn2={handleCancel}
+          />
         </>
       ) : (
         <BtnWrap>
           <NavLink
             to="/signup"
-            className={({ isActive }) => (isActive ? "active" : "")}
+            className={({ isActive }) => (isActive ? 'active' : '')}
           >
             회원가입
           </NavLink>
           <NavLink
             to="/login"
-            className={({ isActive }) => (isActive ? "active" : "")}
+            className={({ isActive }) => (isActive ? 'active' : '')}
           >
             로그인
           </NavLink>
@@ -183,7 +205,6 @@ const Header = () => {
       )}
     </HeaderWrap>
   );
-
 };
 
 export default Header;

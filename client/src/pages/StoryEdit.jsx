61b7a8392fd190ea3b11170d1d2409d82f96e44c
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2023-01-30 11:58:22 +0900

    [feat/fe-readme] 리드미 페이지 완성, 스토리 수정부분 수정 (#226)
    * [FE feat] README 페이지 작업
    
    * [FE feat] #222 리드미 페이지 작업
    
    * [FE misc] #222 스토리 내용 영역 여백 수정
    
    * [FE fix] #222 스토리 내용 수정시 이전 콘텐츠 안 불러오는 에러 수정
    
    * [FE feat] #222 리드미 페이지 제작
    
    * [FE feat] #222 리드미 페이지 pc 완성
    
    * [FE feat] #222 페이지 이미지  팝업 기능 추가
    
    * [FE feat] #222 모바일 반응형 작업 완료
    
    * [FE design] #222 이미지 팝업 색상 및 크기 조정

--- client/src/pages/StoryEdit.jsx
+++ client/src/pages/StoryEdit.jsx
@@ -7,94 +7,95 @@ import { useNavigate, useParams } from "react-router-dom";
 import Popup from "../components/Popup";
 
 const TextArea = styled.div`
-  margin: 24px 0 16px 0;
+	margin: 24px 0 16px 0;
 `;
 
 const StoryEdit = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [userData, setUserData] = useState({});
-  const [content, setContent] = useState("");
-  const navigate = useNavigate();
-  const params = useParams();
-  const loginInfo = useSelector((state) => state.islogin.login);
-  const memberId = loginInfo.memberId;
-  const accessToken = loginInfo.accessToken;
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/api/members/${memberId}`)
-      .then((res) => {
-        setUserData(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+	const [isOpen, setIsOpen] = useState(false);
+	const [userData, setUserData] = useState({});
+	const [content, setContent] = useState("");
+	const navigate = useNavigate();
+	const params = useParams();
+	const loginInfo = useSelector((state) => state.islogin.login);
+	const memberId = loginInfo.memberId;
+	const accessToken = loginInfo.accessToken;
+	useEffect(() => {
+		axios
+			.get(`${process.env.REACT_APP_API_URL}/api/members/${memberId}`)
+			.then((res) => {
+				setUserData(res.data.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/api/boards/${params.boardid}`)
-      .then((res) => {
-        setContent(res.data.data.content);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+		axios
+			.get(`${process.env.REACT_APP_API_URL}/api/boards/${params.boardid}`)
+			.then((res) => {
+				setContent(res.data.data.content);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-  //입력
-  const handleContentOnChange = (e) => {
-    setContent(e.currentTarget.value);
-  };
-  //제출
-  const handlePatch = () => {
-    setIsOpen((prev) => !prev);
-    document.body.style.overflow = "hidden";
-  };
+	//입력
+	const handleContentOnChange = (e) => {
+		setContent(e.currentTarget.value);
+	};
+	//제출
+	const handlePatch = () => {
+		setIsOpen((prev) => !prev);
+		document.body.style.overflow = "hidden";
+	};
 
-  const handleStoryEditSubmit = () => {
-    axios
-      .patch(
-        `${process.env.REACT_APP_API_URL}/api/boards/${params.boardid}`,
-        { content: content },
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      )
-      .then((res) => {
-        setIsOpen((prev) => !prev);
-        navigate("/story");
-        document.body.style.overflow = "unset";
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+	const handleStoryEditSubmit = () => {
+		axios
+			.patch(
+				`${process.env.REACT_APP_API_URL}/api/boards/${params.boardid}`,
+				{ content: content },
+				{
+					headers: {
+						Authorization: `Bearer ${accessToken}`,
+					},
+				}
+			)
+			.then((res) => {
+				setIsOpen((prev) => !prev);
+				navigate("/story");
+				document.body.style.overflow = "unset";
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  return (
-    <PostPatch
-      image={userData.profileImage}
-      nickname={userData.nickname}
-      identifier={userData.identifier}
-      button1="수정완료"
-      button2="취소"
-      link={-1}
-      handleSubmit={handlePatch}
-    >
-      <TextArea>
-        <label>내용</label>
-        <textarea
-          placeholder="스토리 내용만 수정만 가능합니다"
-          onChange={(e) => handleContentOnChange(e)}
-        ></textarea>
-      </TextArea>
-      <Popup
-        isOpen={isOpen}
-        title="스토리 수정"
-        content="스토리 수정이 완료되었습니다."
-        button1="확인"
-        handleBtn1={handleStoryEditSubmit}
-      />
-    </PostPatch>
-  );
+	return (
+		<PostPatch
+			image={userData.profileImage}
+			nickname={userData.nickname}
+			identifier={userData.identifier}
+			button1="수정완료"
+			button2="취소"
+			link={-1}
+			handleSubmit={handlePatch}
+		>
+			<TextArea>
+				<label>내용</label>
+				<textarea
+					placeholder="스토리 내용만 수정만 가능합니다"
+					onChange={(e) => handleContentOnChange(e)}
+					defaultValue={content}
+				></textarea>
+			</TextArea>
+			<Popup
+				isOpen={isOpen}
+				title="스토리 수정"
+				content="스토리 수정이 완료되었습니다."
+				button1="확인"
+				handleBtn1={handleStoryEditSubmit}
+			/>
+		</PostPatch>
+	);
 };
 export default StoryEdit;
